426732e9a0479cf579c0136b1f826937fb65aeb1
jianyong <jianyong.me@gmail.com>
2015-08-03 21:40:10 +0800

    Update pd_shell.sh
    for循环

--- scripts/pd_shell.sh
+++ scripts/pd_shell.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
 
+read name
+
+if [ $name == 'jiang']
+then
+  echo "$name jianyong"
+else
+  echo "$name, Hello World!"
+fi
+
+case "$name" in
+  0) echo "$name 0000000";;
+  jiang) echo "$name jianyong;";;
+esac;
+
+# 遍历数组
+for v in {1..4}
+do
+  echo "$v is ok!"
+done;
+
+# 传统的for循环
+for ((a=1; a<=3; a++))
+do
+  echo "$a is ok!"
+done;
+
+
+
